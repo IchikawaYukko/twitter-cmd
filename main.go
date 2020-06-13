00c0c739ee3307812ac12056529ee11fd2482aa3
市川ゆり子/IchikawaYuriko <ichikawayuriko@yahoo.co.jp>
2020-06-13 23:20:02 +0900

    Add image filename parser
    Signed-off-by: 市川ゆり子/IchikawaYuriko <ichikawayuriko@yahoo.co.jp>

--- main.go
+++ main.go
@@ -15,7 +15,10 @@ func main() {
 		Exit(255)
 	}
 
-	tweet, err := api.PostTweet(parse_message(Args), nil)
+	message, files := parse_message(Args)
+	Println(files)
+
+	tweet, err := api.PostTweet(message, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -23,15 +26,27 @@ func main() {
 	Print(tweet.Text)
 }
 
-func parse_message(args []string) string {
+func parse_message(args []string) (string, []string) {
 	var text string = ""
+	var image_filename []string
+	var image_flag bool
+
 	for i, v := range args {
 		if i == 0 {
 			continue	// skip first arg value (It is name of executable)
 		}
-		text += " " + v
+		if v == "-d" {	// if -d flag found, next args is image filename
+			image_flag = true
+			continue
+		}
+		if image_flag == true {
+			image_filename = append(image_filename, v)
+			image_flag = false
+		} else {
+			text += " " + v
+		}
 	}
-	return text
+	return text, image_filename
 }
 
 func GetTwitterApi() *anaconda.TwitterApi {
